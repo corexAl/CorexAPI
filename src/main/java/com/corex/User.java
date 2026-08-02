15bc3647435ff2a38881808d8369b0eeb9eb075f
SYOP200 <hello@syop200.com>
2026-08-01 21:53:02 -0400

    Create User class with id and username fields

---
+++ src/main/java/com/corex/User.java
@@ -0,0 +1,35 @@
+package com.corex;
+
+
+public class User {
+
+    private String id;
+
+    private String username;
+
+
+    public User(
+        String id,
+        String username
+    ) {
+
+        this.id = id;
+        this.username = username;
+
+    }
+
+
+    public String getId() {
+
+        return id;
+
+    }
+
+
+    public String getUsername() {
+
+        return username;
+
+    }
+
+}
